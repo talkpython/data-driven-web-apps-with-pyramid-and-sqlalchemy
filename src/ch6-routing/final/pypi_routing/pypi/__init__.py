f241399bde2f059f6eeecbfb6dca9988bca9fa55
Michael Kennedy <mikeckennedy@gmail.com>
2018-06-26 15:12:21 -0700

    Final routing demo.

--- src/ch6-routing/final/pypi_routing/pypi/__init__.py
+++ src/ch6-routing/final/pypi_routing/pypi/__init__.py
@@ -23,7 +23,25 @@ def init_routing(config):
     config.add_route('about', '/about')
 
     # package controller
+    config.add_route('popular', '/{num}', custom_predicates=[
+        lambda info, _: info['match'].get('num', '').isdigit()
+    ])
+
     config.add_route('package_details', '/project/{package_name}')
     config.add_route('package_details/', '/project/{package_name}/')
+
+    config.add_route('releases', '/project/{package_name}/releases')
+    config.add_route('releases/', '/project/{package_name}/releases/')
+
+    config.add_route('release_version', '/project/{package_name}/releases/{release_version}')
+
+    # account controller
+    config.add_route('account_home', '/account')
+    config.add_route('login', '/account/login')
+    config.add_route('register', '/account/register')
+    config.add_route('logout', '/account/logout')
+
+    # cms controller -- VERY END
+    config.add_route('cms_page', '*subpath')
 
     config.scan()
--- src/ch6-routing/final/pypi_routing/pypi/controllers/packages_controller.py
+++ src/ch6-routing/final/pypi_routing/pypi/controllers/packages_controller.py
@@ -4,7 +4,6 @@ import pyramid.httpexceptions as x
 
 
 # /project/{package_name}/
-
 @view_config(route_name='package_details',
              renderer='pypi:templates/packages/details.pt')
 @view_config(route_name='package_details/',
@@ -16,4 +15,49 @@ def details(request: Request):
 
     return {
         'package_name': package_name
+    }
+
+
+# /project/{package_name}/releases
+@view_config(route_name='releases',
+             renderer='pypi:templates/packages/releases.pt')
+@view_config(route_name='releases/',
+             renderer='pypi:templates/packages/releases.pt')
+def releases(request: Request):
+    package_name = request.matchdict.get('package_name')
+    if not package_name:
+        raise x.HTTPNotFound()
+
+    return {
+        'package_name': package_name,
+        'releases': []
+    }
+
+
+# /project/{package_name}/releases/{release_version}
+@view_config(route_name='release_version',
+             renderer='pypi:templates/packages/details.pt')
+def release_version(request: Request):
+    package_name = request.matchdict.get('package_name')
+    release_ver = request.matchdict.get('release_version')
+    if not package_name:
+        raise x.HTTPNotFound()
+
+    return {
+        'package_name': package_name,
+        'release_version': release_ver,
+        'releases': []
+    }
+
+
+# /{num}
+@view_config(route_name='popular',
+             renderer='pypi:templates/packages/details.pt')
+def release_version(request: Request):
+    num = int(request.matchdict.get('num', -1))
+    if not (1 <= num or num <= 10):
+        raise x.HTTPNotFound()
+
+    return {
+        'package_name': "The {}th popular package".format(num)
     }
