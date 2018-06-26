a9034085786653d1e4fc2584126bff5cd6720c64
Michael Kennedy <mikeckennedy@gmail.com>
2018-06-25 18:35:35 -0700

    Basic template example done with fake package data passed.

--- src/ch5-templates/final/pypi_templates/pypi/views.py
+++ src/ch5-templates/final/pypi_templates/pypi/views.py
@@ -1,6 +1,16 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'Python Package Index'}
+def get_test_packages():
+    return [
+        {'name': 'requests', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.0.0'},
+        {'name': 'pyramid', 'version': '1.7.7'},
+    ]
+
+
+@view_config(route_name='home', renderer='templates/home_index.pt')
+def home_index(request):
+    return {
+        'packages': get_test_packages()
+    }
