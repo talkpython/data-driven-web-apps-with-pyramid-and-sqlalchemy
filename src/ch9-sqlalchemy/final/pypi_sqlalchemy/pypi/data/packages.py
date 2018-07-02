5fc2d5e787705c56654bc9f910aa545cd8999739
Michael Kennedy <mikeckennedy@gmail.com>
2018-07-02 09:34:24 -0700

    Package, now with defaults and indexes.

--- src/ch9-sqlalchemy/final/pypi_sqlalchemy/pypi/data/packages.py
+++ src/ch9-sqlalchemy/final/pypi_sqlalchemy/pypi/data/packages.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy as sa
 from pypi.data.modelbase import SqlAlchemyBase
 
@@ -6,18 +8,18 @@ class Package(SqlAlchemyBase):
     __tablename__ = 'packages'
 
     id = sa.Column(sa.String, primary_key=True)
-    created_date = sa.Column(sa.DateTime)
-    summary = sa.Column(sa.String)
-    description = sa.Column(sa.String)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now)
+    summary = sa.Column(sa.String, nullable=True)
+    description = sa.Column(sa.String, nullable=True)
 
-    home_page = sa.Column(sa.String)
-    docs_url = sa.Column(sa.String)
-    package_url = sa.Column(sa.String)
+    home_page = sa.Column(sa.String, nullable=True)
+    docs_url = sa.Column(sa.String, nullable=True)
+    package_url = sa.Column(sa.String, nullable=True)
 
-    author_name = sa.Column(sa.String)
-    author_email = sa.Column(sa.String)
+    author_name = sa.Column(sa.String, nullable=True)
+    author_email = sa.Column(sa.String, nullable=True, index=True)
 
-    license = sa.Column(sa.String)
+    license = sa.Column(sa.String, index=True)
 
     # releases
     # maintainers
