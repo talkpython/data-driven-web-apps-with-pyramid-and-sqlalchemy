dd9305c9b3831df0ea5cb7e8329d8a8173b14ba2
Michael Kennedy <mikeckennedy@gmail.com>
2018-07-03 17:00:53 -0700

    Perf (ish) updates -- exploring perf and joins.

--- src/ch10-using-sqlachemy/final/pypi_using_sqlachemy/pypi/services/package_service.py
+++ src/ch10-using-sqlachemy/final/pypi_using_sqlachemy/pypi/services/package_service.py
@@ -1,5 +1,7 @@
 from typing import List, Optional
 
+from sqlalchemy.orm import subqueryload, joinedload
+
 from pypi import DbSession
 from pypi.data.packages import Package
 from pypi.data.releases import Release
@@ -39,4 +41,9 @@ def latest_releases(limit=10) -> List[Package]:
 
 def find_package_by_name(package_name: str) -> Optional[Package]:
     session = DbSession.factory()
-    return session.query(Package).filter(Package.id == package_name).first()
+
+    # .options(subqueryload(Package.releases))
+
+    return session.query(Package) \
+        .filter(Package.id == package_name) \
+        .first()
