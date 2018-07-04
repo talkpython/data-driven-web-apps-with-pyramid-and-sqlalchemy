b34080e81e797e291943c3571189db858030dd8c
Michael Kennedy <mikeckennedy@gmail.com>
2018-07-04 12:46:12 -0700

    Starter code for forms chapter.

---
+++ src/ch12-forms/starter/pypi_forms/pypi/controllers/packages_controller.py
@@ -0,0 +1,49 @@
+from pyramid.view import view_config
+from pyramid.request import Request
+import pyramid.httpexceptions as x
+
+from pypi.services import package_service
+
+
+@view_config(route_name='package_details',
+             renderer='pypi:templates/packages/details.pt')
+@view_config(route_name='package_details/',
+             renderer='pypi:templates/packages/details.pt')
+def details(request: Request):
+    package_name = request.matchdict.get('package_name')
+
+    package = package_service.find_package_by_name(package_name)
+    if not package:
+        raise x.HTTPNotFound()
+
+    latest_version = '0.0.0'
+    latest_release = None
+    if package.releases:
+        latest_release = package.releases[0]
+        latest_version = '{}.{}.{}'.format(
+            latest_release.major_ver,
+            latest_release.minor_ver,
+            latest_release.build_ver
+        )
+
+    return {
+        'package': package,
+        'latest_version': latest_version,
+        'latest_release': latest_release,
+        'release_version': latest_version,
+        'maintainers': [],
+        'is_latest': True
+    }
+
+
+# /{num}
+@view_config(route_name='popular',
+             renderer='pypi:templates/packages/details.pt')
+def popular(request: Request):
+    num = int(request.matchdict.get('num', -1))
+    if not (1 <= num or num <= 10):
+        raise x.HTTPNotFound()
+
+    return {
+        'package_name': "The {}th popular package".format(num)
+    }
