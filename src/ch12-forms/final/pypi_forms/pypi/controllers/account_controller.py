c0ed4ba2a266725765cce0ca9dd8976f3de6398b
Michael Kennedy <mikeckennedy@gmail.com>
2018-07-06 07:55:48 -0700

    Let's start from less.

--- src/ch12-forms/final/pypi_forms/pypi/controllers/account_controller.py
+++ src/ch12-forms/final/pypi_forms/pypi/controllers/account_controller.py
@@ -3,7 +3,6 @@ from pyramid.view import view_config
 
 # ################### INDEX #################################
 
-
 @view_config(route_name='account_home',
              renderer='pypi:templates/account/index.pt')
 def index(request):
