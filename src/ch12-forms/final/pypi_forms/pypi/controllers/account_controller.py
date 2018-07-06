6b09170e18176e7727177ef21d1a85289af17ce7
Michael Kennedy <mikeckennedy@gmail.com>
2018-07-06 08:01:42 -0700

    Basic register get/post/redirect and form in place.

--- src/ch12-forms/final/pypi_forms/pypi/controllers/account_controller.py
+++ src/ch12-forms/final/pypi_forms/pypi/controllers/account_controller.py
@@ -5,5 +5,21 @@ from pyramid.view import view_config
 
 @view_config(route_name='account_home',
              renderer='pypi:templates/account/index.pt')
-def index(request):
+def index(_):
+    return {}
+
+
+# ################### REGISTER #################################
+
+@view_config(route_name='register',
+             renderer='pypi:templates/account/register.pt',
+             request_method='GET')
+def register_get(_):
+    return {}
+
+
+@view_config(route_name='register',
+             renderer='pypi:templates/account/register.pt',
+             request_method='POST')
+def register_post(request):
     return {}
