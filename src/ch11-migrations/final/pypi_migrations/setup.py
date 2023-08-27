5ff5b74f1ff9b8e137729a881e9032062469e94a
Michael Kennedy <michael@talkpython.fm>
2023-08-27 14:00:35 -0700

    Update dependencies for issue with pyramid + Python 3.11.

--- src/ch11-migrations/final/pypi_migrations/setup.py
+++ src/ch11-migrations/final/pypi_migrations/setup.py
@@ -11,11 +11,11 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = [
     'plaster_pastedeploy==1.0.1',
     'progressbar2==4.2.0',
-    'pyramid==2.0.1',
+    'pyramid==2.0.2',
     'pyramid_chameleon==0.3',
     'pyramid_debugtoolbar==4.10',
     'python-dateutil==2.8.2',
-    'sqlalchemy==2.0.19',
+    'sqlalchemy==2.0.20',
     'waitress==2.1.2',
 ]
 
